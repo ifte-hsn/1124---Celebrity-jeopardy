ff20dc7a1333c96fbb24939afc1a1f65e34b14d6
Iftekhar Hossain <ifte.hsn@gmail.com>
2018-11-14 13:47:22 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 1124---Celebrity-jeopardy
+https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=3565
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char str[100000];
+
+    while(gets(str)){
+       puts(str);
+    }
+
+    return 0;
+}
